40ede1ccb84835629f4f418b2e4efd96c11d8c19
emptyfs <vanil01@bk.ru>
2023-12-18 22:51:00 +0300

    Anton's version

--- prototype/version 1.1/DatabaseController.py
+++ prototype/version 1.1/DatabaseController.py
@@ -20,18 +20,21 @@ class DatabaseController:
 
     def export(self, file_name='test'):
         end_path = file_name + '.graphml'
-        temp_string = "CALL apoc.export.graphml.query('MATCH (n)-[m:LEADS_TO {is_active: 1}]->(r) RETURN n, r, m','" + end_path + "', {})"
+        temp_string = "CALL apoc.export.graphml.query('MATCH (n)-[m:LEADS_TO]->(r) RETURN n, r, m','" + end_path + "', {})"
         self.run_query(temp_string)
         return end_path
 
     def import_(self, file_name='test.graphml'):
-        query_string = "CALL apoc.import.graphml('" + file_name + "', {readLabels:true, storeNodeIds:true})"
-        self.run_query(query_string)       
+        query_string = "MATCH (n) DETACH DELETE n"
 
-        query_string = """MATCH (a)
-                    WITH a.url AS url, COLLECT(a) AS branches
-                    WHERE SIZE(branches) > 1
-                    FOREACH (n IN TAIL(branches) | DETACH DELETE n)"""
+        self.run_query(query_string) 
+
+        query_string = "CALL apoc.import.graphml('" + file_name + "', {readLabels:true, storeNodeIds:true})"      
+
+        #query_string = """MATCH (a)
+        #            WITH a.url AS url, COLLECT(a) AS branches
+        #            WHERE SIZE(branches) > 1
+        #            FOREACH (n IN TAIL(branches) | DETACH DELETE n)"""
         self.run_query(query_string)
 
 
